f2910178b77947a5de3f378582475a68bcf013df
Henry van der Vegte <jan-henry.van-der-vegte@stud.uni-due.de>
2019-04-23 22:28:55 +0200

    add train for dnn classifier

--- experiments/audio/dnn_grouped/dnn_train.py
+++ experiments/audio/dnn_grouped/dnn_train.py
@@ -1,28 +1,62 @@
 import torch
 import torch.nn as nn
 import numpy as np
+from experiments.audio.util import util
 import torch.utils.data as utils
-
-TRAIN_FILE_AUDIO = "C://Users//Henry//Desktop//Masterarbeit//IEMOCAP_audio//split//train.txt"
-MEANS = "C://Users//Henry//Desktop//Masterarbeit//IEMOCAP_audio//experiments//dnn//means.txt"
-STDDEVS = "C://Users//Henry//Desktop//Masterarbeit//IEMOCAP_audio//experiments//dnn//stddevs.txt"
+from collections import Counter
+from itertools import chain
 
 # Device configuration
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
+
+TRAIN_FILE_AUDIO = "C://Users//Henry//Desktop//Masterarbeit//IEMOCAP_audio//split//train.txt"
+DEV_FILE_AUDIO = "C://Users//Henry//Desktop//Masterarbeit//IEMOCAP_audio//split//dev.txt"
+EXPERIMENT_PATH = "C://Users//Henry//Desktop//Masterarbeit//IEMOCAP_audio//experiments//dnn"
+
+label_to_id = {
+    "hap":"0",
+    "exc":"0",
+    "sad":"1",
+    "ang":"2",
+    "neu":"3",
+}
+
+train_vectors, train_labels = util.get_and_norm_train_data(TRAIN_FILE_AUDIO, label_to_id, EXPERIMENT_PATH)
+
+labels_count = len(set(list(label_to_id.values())))
+instances_count = train_vectors.shape[0]
+features_count = train_vectors.shape[1]
+
+print("number of labels: " + str(labels_count))
+print("number of instances: " + str(instances_count))
+print("number of features: " + str(features_count))
+
+print("train vectors shape: " + str(train_vectors.shape))
+print("train labels shape: " + str(train_labels.shape))
+
+
+train_labels = np.array(train_labels).reshape(-1,1)
+print(train_labels.shape)
+#one_hot_encoding = np.zeros((instances_count, labels_count))
+#one_hot_encoding[np.arange(instances_count), train_labels] = 1
+
+
+tensor_x = torch.stack([torch.Tensor(i) for i in train_vectors]) # transform to torch tensors
+
+tensor_y = torch.stack([torch.Tensor(i) for i in train_labels])
+
+my_dataset = utils.TensorDataset(tensor_x,tensor_y) # create your datset
+my_dataloader = utils.DataLoader(my_dataset, batch_size=32) # create your dataloader
+
+print("loaded data")
+
 # Hyper-parameters
-num_classes = 10
 num_epochs = 5
 
 hidden_size = 1024
 batch_size = 32
 learning_rate = 0.01
-
-
-my_x = [np.array([[1.0,2],[3,4]]),np.array([[5.,6],[7,8]])]
-my_y = [np.array([4.]), np.array([2.])]
-
-
 
 
 class Net(nn.Module):
@@ -46,22 +80,23 @@ class Net(nn.Module):
         return out
 
 
-model = Net(12, 5).to(device)
+model = Net(features_count, labels_count).to(device)
 
 # Loss and optimizer
 criterion = nn.CrossEntropyLoss()
-optimizer = torch.ptim.SGD(model.parameters(), lr = learning_rate, momentum=0.9)
+optimizer = torch.optim.SGD(model.parameters(), lr = learning_rate, momentum=0.9)
 
 # Train the model
-total_step = len(train_loader)
+total_step = len(my_dataloader)
+print("start train")
 for epoch in range(num_epochs):
-    for i, (images, labels) in enumerate(train_loader):
+    for i, (train_vectors, labels) in enumerate(my_dataloader):
         # Move tensors to the configured device
-        images = images.reshape(-1, 28 * 28).to(device)
-        labels = labels.to(device)
+        train_vectors = train_vectors.to(device)
+        labels = labels.to(device, dtype=torch.int64).view(-1)
 
         # Forward pass
-        outputs = model(images)
+        outputs = model(train_vectors)
         loss = criterion(outputs, labels)
 
         # Backward and optimize
--- experiments/audio/util/util.py
+++ experiments/audio/util/util.py
@@ -1,4 +1,5 @@
 import numpy as np
+import os
 
 def get_and_norm_train_data(data_path, label_to_id, experiment_path):
     train_file = open(data_path, "r")
@@ -14,9 +15,12 @@ def get_and_norm_train_data(data_path, label_to_id, experiment_path):
         features = split[2]
         if label in label_to_id:
             train_labels.append(int(label_to_id[label]))
-            train_vectors.append(np.array(features.split(',')).astype(np.float))
+            features = features.split(',')
+            features = [float(i) for i in features]
+            train_vectors.append(np.array(features))
 
     train_vectors = np.array(train_vectors)
+    train_labels = np.array(train_labels)
 
     means = train_vectors.mean(axis=0)
     stddevs = train_vectors.std(axis=0)
@@ -26,12 +30,12 @@ def get_and_norm_train_data(data_path, label_to_id, experiment_path):
 
     normed_vectors = (train_vectors - means) / stddevs
 
-    with open(means_out, "w") as f:
+    with open(os.path.join(experiment_path, "means.txt"), "w") as f:
         for mean in means:
             f.write(str(mean) + '\n')
 
-    with open(stddevs_out, "w") as f:
+    with open(os.path.join(experiment_path, "stddevs.txt"), "w") as f:
         for stddev in stddevs:
             f.write(str(stddev) + '\n')
 
-    return normed_vectors
+    return normed_vectors, train_labels
