0fef8bda6e898e42748458745183fd331bd32078
Henry van der Vegte <jan-henry.van-der-vegte@stud.uni-due.de>
2019-04-25 00:02:29 +0200

    start cleaning up naive bayes classifier scripts

--- experiments/text/Naive Bayes/naive_bayes_test.py
+++ experiments/text/Naive Bayes/naive_bayes_test.py
@@ -3,11 +3,11 @@ from sklearn.metrics import recall_score
 from nltk.metrics import ConfusionMatrix, accuracy
 import pickle
 
-TEST_FILE = "C://Users//Henry//Desktop//Masterarbeit//IEMOCAP_txt//split//dev.txt"
-ALL_WORDS = "C://Users//Henry//Desktop//Masterarbeit//IEMOCAP_txt//experiments//Naive Bayes//all_words.txt"
-PICKLE = "C://Users//Henry//Desktop//Masterarbeit//IEMOCAP_txt//experiments//Naive Bayes//naive_bayes.pickle"
+EXAMPLE_TEST_FILE = "C://Users//Henry//Desktop//Masterarbeit//IEMOCAP_txt//split//dev.txt"
+EXAMPLE_ALL_WORDS = "C://Users//Henry//Desktop//Masterarbeit//IEMOCAP_txt//experiments//Naive Bayes//all_words.txt"
+EXAMPLE_PICKLE = "C://Users//Henry//Desktop//Masterarbeit//IEMOCAP_txt//experiments//Naive Bayes//naive_bayes.pickle"
 
-label_to_id = {
+example_label_to_id = {
     "hap":"0",
     "exc":"0",
     "sad":"1",
@@ -21,7 +21,7 @@ def get_all_words(lines):
         all_words.add(line[:-1])
     return all_words
 
-def get_test_data(lines, all_words):
+def get_test_data(lines, all_words, label_to_id):
     test_data = []
     test_labels = []
     for line in lines:
@@ -35,26 +35,46 @@ def get_test_data(lines, all_words):
             test_labels.append(label_to_id[label])
     return test_data, test_labels
 
-words_file = open(ALL_WORDS, "r")
-word_lines = words_file.readlines()
-all_words = get_all_words(word_lines)
+def test(test_file_in, all_words_file_in, pickle_in, label_to_id):
+    words_file = open(all_words_file_in, "r")
+    word_lines = words_file.readlines()
+    all_words = get_all_words(word_lines)
 
-test_file = open(TEST_FILE, "r")
-test_lines = test_file.readlines()
-test_data, test_labels = get_test_data(test_lines, all_words)
+    test_file = open(test_file_in, "r")
+    test_lines = test_file.readlines()
+    test_data, test_labels = get_test_data(test_lines, all_words, label_to_id)
 
-f = open(PICKLE, 'rb')
-classifier = pickle.load(f)
-f.close()
+    f = open(pickle_in, 'rb')
+    classifier = pickle.load(f)
+    f.close()
 
-pred = classifier.classify_many(test_data)
-print(pred)
-print(test_labels)
-print("#########################\n")
-print(recall_score(test_labels, pred, average='macro'))
+    tuples = []
 
-print("#########################\n")
-cm = ConfusionMatrix(test_labels, pred)
-print(cm)
+    for i in range(0,len(test_data)):
+        tuples.append((test_data[i], test_labels[i]))
 
-print("Accuracy:" + str(accuracy(test_labels, pred)))
+    pred_perc = classifier.prob_classify(test_data[0])
+
+    for i in pred_perc.samples():
+        print(i + " " + str(pred_perc.prob(i)))
+
+    print(pred_perc.prob('0'))
+    print(pred_perc.prob('1'))
+    print(pred_perc.prob('2'))
+    print(pred_perc.prob('3'))
+    print(pred_perc.prob('4'))
+    print(pred_perc.prob('5'))
+
+    pred = classifier.prob_classify_many(test_data)
+    print(pred)
+    print(test_labels)
+    print("#########################\n")
+    print(recall_score(test_labels, pred, average='macro'))
+
+    print("#########################\n")
+    cm = ConfusionMatrix(test_labels, pred)
+    print(cm)
+
+    print("Accuracy:" + str(accuracy(test_labels, pred)))
+
+test(EXAMPLE_TEST_FILE, EXAMPLE_ALL_WORDS, EXAMPLE_PICKLE, example_label_to_id)
--- experiments/text/Naive Bayes/naive_bayes_train.py
+++ experiments/text/Naive Bayes/naive_bayes_train.py
@@ -1,11 +1,11 @@
 import nltk
 import pickle
 
-TRAIN_FILE = "C://Users//Henry//Desktop//Masterarbeit//IEMOCAP_txt//split//train.txt"
-ALL_WORDS = "C://Users//Henry//Desktop//Masterarbeit//IEMOCAP_txt//experiments//Naive Bayes//all_words.txt"
-PICKLE = "C://Users//Henry//Desktop//Masterarbeit//IEMOCAP_txt//experiments//Naive Bayes//naive_bayes.pickle"
+EXAMPLE_TRAIN_FILE = "C://Users//Henry//Desktop//Masterarbeit//IEMOCAP_txt//split//train.txt"
+EXAMPLE_ALL_WORDS = "C://Users//Henry//Desktop//Masterarbeit//IEMOCAP_txt//experiments//Naive Bayes//all_words.txt"
+EXAMPLE_PICKLE = "C://Users//Henry//Desktop//Masterarbeit//IEMOCAP_txt//experiments//Naive Bayes//naive_bayes.pickle"
 
-label_to_id = {
+example_label_to_id = {
     "hap":"0",
     "exc":"0",
     "sad":"1",
@@ -21,7 +21,7 @@ def get_all_words(lines):
         all_words.update(tokens)
     return all_words
 
-def get_train_data(lines, all_words):
+def get_train_data(lines, all_words, label_to_id):
     train_data = []
     for line in lines:
         label = line.split('\t')[1]
@@ -32,20 +32,23 @@ def get_train_data(lines, all_words):
             train_data.append(train_instance)
     return train_data
 
-train_file = open(TRAIN_FILE, "r")
-train_lines = train_file.readlines()
+def train(train_file_in, all_words_file_out, pickle_out, label_to_id):
+    train_file = open(train_file_in, "r")
+    train_lines = train_file.readlines()
 
-all_words = get_all_words(train_lines)
+    all_words = get_all_words(train_lines)
 
-with open(ALL_WORDS, "w") as f:
-    for word in all_words:
-        f.write(word + '\n')
+    with open(all_words_file_out, "w") as f:
+        for word in all_words:
+            f.write(word + '\n')
 
-train_data = get_train_data(train_lines, all_words)
+    train_data = get_train_data(train_lines, all_words, label_to_id)
 
-classifier = nltk.NaiveBayesClassifier.train(train_data)
-classifier.show_most_informative_features()
+    classifier = nltk.NaiveBayesClassifier.train(train_data)
+    classifier.show_most_informative_features()
 
-f = open(PICKLE, 'wb')
-pickle.dump(classifier, f)
-f.close()
+    f = open(pickle_out, 'wb')
+    pickle.dump(classifier, f)
+    f.close()
+
+train(EXAMPLE_TRAIN_FILE, EXAMPLE_ALL_WORDS, EXAMPLE_PICKLE, example_label_to_id)
