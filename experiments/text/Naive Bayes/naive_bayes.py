f4220943ea167e9547574fcd1d28c65de7991d6c
Henry van der Vegte <jan-henry.van-der-vegte@stud.uni-due.de>
2019-04-25 23:01:29 +0200

    merge naive bayes train test

--- experiments/text/Naive Bayes/naive_bayes.py
+++ experiments/text/Naive Bayes/naive_bayes.py
@@ -1,9 +1,9 @@
 import nltk
+import pickle
 from sklearn.metrics import recall_score
 from nltk.metrics import ConfusionMatrix, accuracy
-import pickle
 
-EXAMPLE_TEST_FILE = "C://Users//Henry//Desktop//Masterarbeit//IEMOCAP_txt//split//dev.txt"
+EXAMPLE_TRAIN_FILE = "C://Users//Henry//Desktop//Masterarbeit//IEMOCAP_txt//split//train.txt"
 EXAMPLE_ALL_WORDS = "C://Users//Henry//Desktop//Masterarbeit//IEMOCAP_txt//experiments//Naive Bayes//all_words.txt"
 EXAMPLE_PICKLE = "C://Users//Henry//Desktop//Masterarbeit//IEMOCAP_txt//experiments//Naive Bayes//naive_bayes.pickle"
 
@@ -18,6 +18,46 @@ example_label_to_id = {
 def get_all_words(lines):
     all_words = set([])
     for line in lines:
+        utterance = line.split('\t')[2]
+        tokens = nltk.word_tokenize(utterance.lower())
+        all_words.update(tokens)
+    return all_words
+
+def get_train_data(lines, all_words, label_to_id):
+    train_data = []
+    for line in lines:
+        label = line.split('\t')[1]
+        utterance = line.split('\t')[2]
+        if label in label_to_id:
+            tokens = nltk.word_tokenize(utterance.lower())
+            train_instance = ({word: (word in tokens) for word in all_words}, label_to_id[label])
+            train_data.append(train_instance)
+    return train_data
+
+def train(train_file_in, all_words_file_out, pickle_out, label_to_id):
+    train_file = open(train_file_in, "r")
+    train_lines = train_file.readlines()
+
+    all_words = get_all_words(train_lines)
+
+    with open(all_words_file_out, "w") as f:
+        for word in all_words:
+            f.write(word + '\n')
+
+    train_data = get_train_data(train_lines, all_words, label_to_id)
+
+    classifier = nltk.NaiveBayesClassifier.train(train_data)
+    classifier.show_most_informative_features()
+
+    f = open(pickle_out, 'wb')
+    pickle.dump(classifier, f)
+    f.close()
+
+def get_all_words_from_file(all_words_file):
+    words_file = open(all_words_file, "r")
+    word_lines = words_file.readlines()
+    all_words = set([])
+    for line in word_lines:
         all_words.add(line[:-1])
     return all_words
 
@@ -36,9 +76,7 @@ def get_test_data(lines, all_words, label_to_id):
     return test_data, test_labels
 
 def test(test_file_in, all_words_file_in, pickle_in, label_to_id):
-    words_file = open(all_words_file_in, "r")
-    word_lines = words_file.readlines()
-    all_words = get_all_words(word_lines)
+    all_words = get_all_words_from_file(all_words_file_in)
 
     test_file = open(test_file_in, "r")
     test_lines = test_file.readlines()
@@ -60,10 +98,8 @@ def test(test_file_in, all_words_file_in, pickle_in, label_to_id):
 
     print("Accuracy:" + str(accuracy(test_labels, pred)))
 
-def test_get_probability_scores(test_file_in, all_words_file_in, pickle_in, label_to_id):
-    words_file = open(all_words_file_in, "r")
-    word_lines = words_file.readlines()
-    all_words = get_all_words(word_lines)
+def eval_get_probability_scores(test_file_in, all_words_file_in, pickle_in, label_to_id):
+    all_words = get_all_words_from_file(all_words_file_in)
 
     test_file = open(test_file_in, "r")
     test_lines = test_file.readlines()
@@ -82,7 +118,4 @@ def test_get_probability_scores(test_file_in, all_words_file_in, pickle_in, labe
         probabilities.append(label_probs)
     return probabilities
 
-#test(EXAMPLE_TEST_FILE, EXAMPLE_ALL_WORDS, EXAMPLE_PICKLE, example_label_to_id)
-
-test_get_probability_scores(EXAMPLE_TEST_FILE, EXAMPLE_ALL_WORDS, EXAMPLE_PICKLE, example_label_to_id)
-
+train(EXAMPLE_TRAIN_FILE, EXAMPLE_ALL_WORDS, EXAMPLE_PICKLE, example_label_to_id)
