705f8bf3c23166c83546680439ef4523533a83f2
Henry van der Vegte <jan-henry.van-der-vegte@stud.uni-due.de>
2019-05-04 17:42:02 +0200

    refactor svm experiment

--- classification/experiments/fusion/experiment_classify_fusion_svm.py
+++ classification/experiments/fusion/experiment_classify_fusion_svm.py
@@ -1,5 +1,6 @@
 from classification.audio import svm
 from classification.util.experiments_util import *
+import classification.util.data_loader_txt as data_loader
 from classification.util.global_vars import *
 import os
 
@@ -18,5 +19,10 @@ label_to_id = {
 
 experiment_dir, logger = create_experiment(experiments_folder, label_to_id, "classify_fusion_svm", use_timestamp=True)
 
-svm.train_from_file(fusion_train, experiment_dir, label_to_id, logger)
-svm.test_from_file(fusion_dev, experiment_dir, label_to_id, logger)
+train_vectors, train_labels = data_loader.get_train_data(fusion_train, label_to_id, experiment_dir, False)
+
+dev_vectors, dev_labels = data_loader.get_test_data(fusion_dev, label_to_id, experiment_dir, False)
+
+
+svm.train(train_labels, train_vectors, experiment_dir, logger)
+svm.test(dev_labels, dev_vectors, experiment_dir, logger)
