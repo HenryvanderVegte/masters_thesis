19ebfe2185db4f8fd6d20a55ed75fe7cd70f28c9
Henry van der Vegte <jan-henry.van-der-vegte@stud.uni-due.de>
2019-05-13 20:08:11 +0200

    log models while experiment running

--- classification/experiments/text/experiment_classify_embedding_vectors_rnn.py
+++ classification/experiments/text/experiment_classify_embedding_vectors_rnn.py
@@ -10,7 +10,7 @@ TRAIN_EMBEDDINGS = os.path.join(ROOT_FOLDER, "IEMOCAP_txt//embeddings//2-dim//tr
 DEV_EMBEDDINGS_LABELS = os.path.join(ROOT_FOLDER, "IEMOCAP_txt//embeddings//2-dim//dev_embeddings.txt")
 DEV_EMBEDDINGS = os.path.join(ROOT_FOLDER, "IEMOCAP_txt//embeddings//2-dim//dev_embeddings.npy")
 
-EXPERIMENTS_FOLDER = "C://Users//Henry//Desktop//Masterarbeit//IEMOCAP_txt//classification//"
+EXPERIMENTS_FOLDER = "C://Users//Henry//Desktop//Masterarbeit//IEMOCAP_txt//experiments//"
 
 label_to_id = {
     "hap":"0",
@@ -21,19 +21,18 @@ label_to_id = {
 }
 
 params = {
-    "sequence_length":30,
+    "sequence_length":40,
+    "batch_size": 16,
     "layers": 2,
-    "epochs": 200,
+    "epochs": 1000,
 }
 
-experiment_dir, logger = create_experiment(EXPERIMENTS_FOLDER, label_to_id, "classify_word_embeddings_dnn", use_timestamp=True)
+experiment_dir, logger = create_experiment(EXPERIMENTS_FOLDER, label_to_id, "classify_word_embeddings_rnn", use_timestamp=True)
 
 train_labels, train_features = data_loader.load_dict_from_binary(TRAIN_EMBEDDINGS_LABELS, TRAIN_EMBEDDINGS, label_to_id)
 train_dataset = create_sequence_dataset(train_features, train_labels, params["sequence_length"])
 
-dev_labels, dev_features = data_loader.load_from_binary(DEV_EMBEDDINGS_LABELS, DEV_EMBEDDINGS, label_to_id)
+dev_labels, dev_features = data_loader.load_dict_from_binary(DEV_EMBEDDINGS_LABELS, DEV_EMBEDDINGS, label_to_id)
 dev_dataset = create_sequence_dataset(dev_features, dev_labels, params["sequence_length"])
 
 rnn_pretrained_embeddings.train(train_dataset, dev_dataset, experiment_dir, label_to_id, logger, params)
-
-
--- classification/text/rnn_pretrained_embeddings.py
+++ classification/text/rnn_pretrained_embeddings.py
@@ -7,20 +7,17 @@ from classification.util.experiments_util import log_metrics
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
-class SentimentLSTM(nn.Module):
+class SentimentRNN(nn.Module):
     def __init__(self, output_size, embedding_dim, n_layers):
         super().__init__()
 
-        self.output_size = output_size
-        self.n_layers = n_layers
-
-        self.lstm = nn.LSTM(embedding_dim, embedding_dim, n_layers, dropout=0.4)
+        self.rnn = nn.GRU(input_size=embedding_dim, hidden_size=embedding_dim, num_layers=n_layers, dropout=0.4)
 
         self.fc = nn.Linear(embedding_dim, output_size)
         self.relu = nn.ReLU()
 
     def forward(self, x):
-        out, _ = self.lstm(x)
+        out, _ = self.rnn(x)
         out = out[-1]
         out = self.fc(out)
         out = self.relu(out)
@@ -35,11 +32,11 @@ def train(train_dataset, dev_dataset, experiment_path, label_to_id, logger, para
     embedding_dim = train_loader.dataset[0][0].size()[1]
     n_layers = params["layers"]
 
-    net = SentimentLSTM(labels_count, embedding_dim, n_layers).to(device)
+    model = SentimentRNN(labels_count, embedding_dim, n_layers).to(device)
 
     # Loss and optimizer
     criterion = nn.CrossEntropyLoss()
-    optimizer = optim.Adam(net.parameters())
+    optimizer = optim.Adam(model.parameters())
 
     # Train the model
     total_step = len(train_loader)
@@ -54,39 +51,83 @@ def train(train_dataset, dev_dataset, experiment_path, label_to_id, logger, para
 
             labels = labels.to(device, dtype=torch.int64).view(-1)
 
-            net.zero_grad()
+            model.zero_grad()
 
-            output = net(inputs)
+            output = model(inputs)
 
             loss = criterion(output, labels)
             loss.backward()
 
             optimizer.step()
 
-            if (i + 1) % 20 == 0:
-                print('Epoch [{}/{}], Step [{}/{}], Loss: {:.4f}'
-                      .format(e + 1, 30, i + 1, total_step, loss.item()))
-
-        predictions = []
-        gold = []
+        train_loss = 0
         with torch.no_grad():
             for inputs, labels in dev_loader:
                 inputs = inputs.to(device)
                 inputs = torch.transpose(inputs, 0, 1)
                 labels = labels.to(device, dtype=torch.int64).view(-1)
 
-                output = net(inputs)
+                output = model(inputs)
+                train_loss = criterion(output, labels).item()
 
-                _, predicted = torch.max(output.data, 1)
-                predictions += predicted.data.tolist()
-                gold += labels.data.tolist()
+        test_loss = 0
+        with torch.no_grad():
+            for inputs, labels in dev_loader:
+                inputs = inputs.to(device)
+                inputs = torch.transpose(inputs, 0, 1)
+                labels = labels.to(device, dtype=torch.int64).view(-1)
 
-        log_metrics(gold, predictions, logger)
+                output = model(inputs)
+                test_loss = criterion(output, labels).item()
 
-    # export pytorch model
-    model_path = os.path.join(experiment_path, "dnn_audio.pth")
-    torch.save(net.state_dict(), model_path)
+        print('{}; {:.0f} {:.4f}; {:.4f}'.format(e + 1, params["epochs"], train_loss, test_loss))
 
-    onnx_model_path = os.path.join(experiment_path, "dnn_audio.onnx")
+        # export pytorch model
+        if (e+1) % 10 == 0:
+            # export pytorch model
+            epoch_path = os.path.join(experiment_path, "epoch_" + str(e))
+            os.mkdir(epoch_path)
+            model_path = os.path.join(epoch_path,  "rnn.pth")
+            torch.save(model.state_dict(), model_path)
+
+            onnx_model_path = os.path.join(epoch_path,  "rnn.onnx")
+            dummy_input = torch.randn(1, seq_length, embedding_dim, device='cuda')
+            torch.onnx.export(model, dummy_input, onnx_model_path, verbose=False)
+
+    model_path = os.path.join(experiment_path, "rnn.pth")
+    torch.save(model.state_dict(), model_path)
+
+    onnx_model_path = os.path.join(experiment_path, "rnn.onnx")
     dummy_input = torch.randn(1, seq_length, embedding_dim, device='cuda')
-    torch.onnx.export(net, dummy_input, onnx_model_path, verbose=True)
+    torch.onnx.export(model, dummy_input, onnx_model_path, verbose=True)
+
+
+def test(dev_dataset, experiment_path, label_to_id, logger, params):
+    dev_loader = utils.DataLoader(dev_dataset, shuffle=False, batch_size=params["batch_size"])
+
+    labels_count = len(set(list(label_to_id.values())))
+    seq_length = dev_loader.dataset[0][0].size()[0]
+    embedding_dim = dev_loader.dataset[0][0].size()[1]
+    n_layers = params["layers"]
+
+    model_path = os.path.join(experiment_path, "rnn.pth")
+    model = SentimentRNN(labels_count, embedding_dim, n_layers).to(device)
+    model.load_state_dict(torch.load(model_path))
+    model.eval()
+    logger.info("Loaded model from" + model_path)
+
+    predictions = []
+    gold = []
+    with torch.no_grad():
+        for inputs, labels in dev_loader:
+            inputs = inputs.to(device)
+            inputs = torch.transpose(inputs, 0, 1)
+            labels = labels.to(device, dtype=torch.int64).view(-1)
+
+            output = model(inputs)
+
+            _, predicted = torch.max(output.data, 1)
+            predictions += predicted.data.tolist()
+            gold += labels.data.tolist()
+
+    log_metrics(gold, predictions, logger)
