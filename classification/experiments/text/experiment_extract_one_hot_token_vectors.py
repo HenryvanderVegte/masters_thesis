6d829e62f5dc6e4a227e8eb83b696e68db7fc822
Henry van der Vegte <jan-henry.van-der-vegte@stud.uni-due.de>
2019-05-15 00:13:53 +0200

    add experiment for one hot vector extraction

---
+++ classification/experiments/text/experiment_extract_one_hot_token_vectors.py
@@ -0,0 +1,36 @@
+from classification.util.experiments_util import *
+from classification.util.global_vars import *
+from classification.text.word_embeddings import *
+import os
+
+
+experiments_folder = os.path.join(ROOT_FOLDER, "IEMOCAP_txt//experiments")
+
+
+train_txt = os.path.join(ROOT_FOLDER, "IEMOCAP//features//text//utterances//train.txt")
+dev_txt = os.path.join(ROOT_FOLDER, "IEMOCAP//features//text//utterances//dev.txt")
+test_txt = os.path.join(ROOT_FOLDER, "IEMOCAP//features//text//utterances//test.txt")
+
+label_to_id = {
+    "hap":"0",
+    "exc":"0",
+    "sad":"1",
+    "ang":"2",
+    "neu":"3",
+}
+
+experiment_dir, logger = create_experiment(experiments_folder, label_to_id, "extract_one_hot_vectors_with_time", use_timestamp=True)
+
+dict = build_one_hot_dict(train_txt, 3000)
+
+train_one_hot_path = os.path.join(experiment_dir, 'train_one_hot.npy')
+train_labels_path = os.path.join(experiment_dir, 'train_one_hot.txt')
+extract_one_hot_vectors_with_time(train_txt, dict, train_labels_path, train_one_hot_path)
+
+dev_one_hot_path = os.path.join(experiment_dir, 'dev_one_hot.npy')
+dev_labels_path = os.path.join(experiment_dir, 'dev_one_hot.txt')
+extract_one_hot_vectors_with_time(dev_txt, dict, dev_labels_path, dev_one_hot_path)
+
+test_one_hot_path = os.path.join(experiment_dir, 'test_one_hot.npy')
+test_labels_path = os.path.join(experiment_dir, 'test_one_hot.txt')
+extract_one_hot_vectors_with_time(test_txt, dict, test_labels_path, test_one_hot_path)
--- classification/text/word_embeddings.py
+++ classification/text/word_embeddings.py
@@ -1,8 +1,13 @@
 import numpy as np
-
 from nltk.tokenize import RegexpTokenizer
-
+from collections import Counter
+from string import punctuation
 tokenizer = RegexpTokenizer(r'\w+')
+
+def __extract_tokens_from_utterance(utterance):
+    utterance = ''.join([c for c in utterance if c not in punctuation])
+    tokens = tokenizer.tokenize(utterance.lower())
+    return tokens
 
 def extract_word_embeddings(file_in, model, label_file_out, vector_file_out, max_token_length):
     vector_size = model.vector_size
@@ -77,4 +82,80 @@ def extract_word_embeddings_with_time(file_in, model, label_file_out, dict_file_
 
     np.save(dict_file_out, instance_dict)
 
+def extract_one_hot_vectors_with_time(file_in, one_hot_dict, label_file_out, dict_file_out):
+    """
+    Extracts the feature vectors into a dictionary with dict[id] = (tokens, vectors).
+    Vectors are one-hot-encoded based on a dictionary.
+    :param file_in:
+    :param one_hot_dict:
+    :param label_file_out:
+    :param dict_file_out:
+    :return:
+    """
 
+    one_hot_dim = len(one_hot_dict["OOV"])
+
+    file_in = open(file_in, "r")
+    file_in_lines = file_in.readlines()
+
+    label_file = ""
+    instance_dict = {}
+
+    for i in range(len(file_in_lines)):
+        split = file_in_lines[i].split("\t")
+
+        id = split[0]
+        label = split[1]
+        label_file += id + "\t" + label + "\n"
+
+        utterance = file_in_lines[i].split('\t')[2][:-1]
+        tokens = __extract_tokens_from_utterance(utterance)
+
+        if len(tokens) == 0:
+            continue
+
+        instance_matrix = np.zeros(shape=(len(tokens), one_hot_dim))
+        j = 0
+        for token in tokens:
+            instance_matrix[j,:] = one_hot_dict[token] if token in one_hot_dict else one_hot_dict["OOV"]
+            j += 1
+
+        instance_dict[id] = instance_matrix
+
+    with open(label_file_out, "w") as f:
+        f.write(label_file)
+
+    np.save(dict_file_out, instance_dict)
+
+def build_one_hot_dict(file_in, vocab_size):
+    """
+    Creates a dictionary containing one-hot-encoded tokens.
+    Items in dictionary is vocab_size + 2 (for out of vocabulary (OOV) and end of utterance (EOU))
+    :param file_in:
+    :param vocab_size:
+    :return:
+    """
+    file_in = open(file_in, "r")
+    file_in_lines = file_in.readlines()
+
+    all_tokens = []
+    for line in file_in_lines:
+        utterance = line.split("\t")[2][:-1]
+        tokens = __extract_tokens_from_utterance(utterance)
+        all_tokens += tokens
+    counter = Counter(all_tokens)
+
+    dict = {}
+    for i, (word, _) in enumerate(counter.most_common(vocab_size)):
+        one_hot = np.zeros((vocab_size + 2))
+        one_hot[i] = 1
+        dict[word] = one_hot
+
+    one_hot = np.zeros((vocab_size + 2))
+    one_hot[vocab_size] = 1
+    dict['OOV'] = one_hot
+
+    one_hot = np.zeros((vocab_size + 2))
+    one_hot[vocab_size + 1] = 1
+    dict['EOU'] = one_hot
+    return dict
