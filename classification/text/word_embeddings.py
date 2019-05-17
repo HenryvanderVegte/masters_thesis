09e5b04ba0bc26fc33ef526504852d76f4d49e8c
Henry van der Vegte <jan-henry.van-der-vegte@stud.uni-due.de>
2019-05-17 19:33:23 +0200

    add token id extraction

--- classification/text/word_embeddings.py
+++ classification/text/word_embeddings.py
@@ -159,3 +159,71 @@ def build_one_hot_dict(file_in, vocab_size):
     one_hot[vocab_size + 1] = 1
     dict['EOU'] = one_hot
     return dict
+
+def extract_token_to_id_vectors_with_time(file_in, token_dict, label_file_out, dict_file_out):
+    """
+    Extracts the feature vectors into a dictionary with one element for each token (based on the :token_dict:)
+    :param file_in:
+    :param token_dict:
+    :param label_file_out:
+    :param dict_file_out:
+    :return:
+    """
+
+    file_in = open(file_in, "r")
+    file_in_lines = file_in.readlines()
+
+    label_file = ""
+    instance_dict = {}
+
+    for i in range(len(file_in_lines)):
+        split = file_in_lines[i].split("\t")
+
+        id = split[0]
+        label = split[1]
+        label_file += id + "\t" + label + "\n"
+
+        utterance = file_in_lines[i].split('\t')[2][:-1]
+        tokens = __extract_tokens_from_utterance(utterance)
+
+        if len(tokens) == 0:
+            continue
+
+        instance_vector = np.zeros(shape=(len(tokens)))
+        j = 0
+        for token in tokens:
+            instance_vector[j] = token_dict[token] if token in token_dict else token_dict["OOV"]
+            j += 1
+
+        instance_dict[id] = instance_vector
+
+    with open(label_file_out, "w") as f:
+        f.write(label_file)
+
+    np.save(dict_file_out, instance_dict)
+
+def build_token_to_id_dict(file_in, vocab_size):
+    """
+    Creates a dictionary mapping the :vocab_size: most common words in the training data to ids
+    :vocab_size: + 2 items are in the dictionary (for out of vocabulary (OOV) and end of utterance (EOU)
+    :param file_in:
+    :param vocab_size:
+    :return:
+    """
+    file_in = open(file_in, "r")
+    file_in_lines = file_in.readlines()
+
+    all_tokens = []
+    for line in file_in_lines:
+        utterance = line.split("\t")[2][:-1]
+        tokens = __extract_tokens_from_utterance(utterance)
+        all_tokens += tokens
+    counter = Counter(all_tokens)
+
+    dict = {}
+    for i, (word, _) in enumerate(counter.most_common(vocab_size)):
+        dict[word] = i
+
+    dict['OOV'] = vocab_size
+    dict['EOU'] = vocab_size + 1
+    return dict
