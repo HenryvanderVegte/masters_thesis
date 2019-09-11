6b6aa113a930cce1d3b3b2d8bace8dff58f3664f
Henry van der Vegte <jan-henry.van-der-vegte@stud.uni-due.de>
2019-09-11 18:04:36 +0200

    Update dataset_utils.py

--- utils/dataset_utils.py
+++ utils/dataset_utils.py
@@ -141,6 +141,73 @@ def create_sequence_dataset_from_metadata(metadata, features_dict, class_groups,
 
     return dataset
 
+def create_multilabel_sequence_dataset_from_metadata(metadata, features_dict, class_groups, folds, max_seq_length = None):
+    """
+    Loads sequence data into a TensorDataset,
+    if max_seq_length is set, sequences longer than seq_length will be cut.
+    sequences shorter than max_seq_length will be zero-padded.
+    The label will be a vector of all classes with the percentage of each selected class.
+    If label by annotator 1 is : [0,1,0,0]
+    and the label by annotator 2 is: [0,0,1,0],
+    the label vector will be [0,0.5,0.5,0].
+    Returns a TensorDataset which contains features, labels, sequence lengths, and ids
+    :param feature_dict:
+    :param label_dict:
+    :param max_seq_length:
+    :return:
+    """
+    label_count = len(set(list(class_groups.values())))
+    fl = []
+    majority_labels = []
+    all_labels = []
+    lengths = []
+    ids = []
+    for instance in metadata:
+        if instance["Label"] not in class_groups or int(instance["Fold"]) not in folds:
+            continue
+
+        majority_label = class_groups[instance["Label"]]
+
+        if instance['Name'] not in features_dict:
+            print('No features for:' + instance['Name'])
+            continue
+
+        features = features_dict[instance['Name']]
+
+        if len(features) == 0:
+            print('No features for:' + instance['Name'])
+            continue
+
+        label_dist = [ int(class_groups[x]) for x in instance['Labels_All'].split(';') if x in class_groups]
+        one_hot_vecs = np.eye(label_count)[np.array(label_dist).reshape(-1)]
+        all_labels_vec = np.sum(one_hot_vecs, axis=0) #/ len(label_dist)
+
+        if max_seq_length is None:
+            fl.append(torch.stack([torch.Tensor(i) for i in features]))
+        else:
+            fl.append(torch.stack([torch.Tensor(i) for i in features[:max_seq_length, :]]))
+
+        length = features.shape[0] if max_seq_length is None else min(features.shape[0], max_seq_length)
+        lengths.append(length)
+        majority_labels.append(majority_label)
+        all_labels.append(all_labels_vec)
+        ids.append(int(instance["Id"]))
+
+    majority_labels = np.array(majority_labels).reshape(-1,1)
+    lengths = np.array(lengths).reshape(-1,1)
+    ids = np.array(ids).reshape(-1,1)
+    padded_features = pad_sequence(fl)
+
+    majority_labels = torch.stack([torch.Tensor(i) for i in majority_labels])
+    all_labels = torch.stack([torch.Tensor(i) for i in all_labels])
+    lengths = torch.stack([torch.Tensor(i) for i in lengths])
+    ids = torch.stack([torch.Tensor(i) for i in ids])
+
+    padded_features = torch.transpose(padded_features, 0, 1)
+    dataset = utils.TensorDataset(padded_features, majority_labels, all_labels, lengths, ids)
+
+    return dataset
+
 def create_dataset_from_metadata(metadata, features_dict, class_groups, folds):
     """
     Loads data into a TensorDataset,
