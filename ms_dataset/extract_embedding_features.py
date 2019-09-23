5a817a070d5592855defd5c3b95b98ecf272b521
Henry van der Vegte <jan-henry.van-der-vegte@stud.uni-due.de>
2019-09-24 00:24:28 +0200

    preprocessing refactor

--- ms_dataset/extract_embedding_features.py
+++ ms_dataset/extract_embedding_features.py
@@ -1,2 +1,40 @@
 from utils.experiments_util import *
 from classification.util.global_vars import *
+import re
+from classification.util.global_vars import *
+from classification.text.word_embeddings import *
+import gensim
+import os
+import tqdm
+import numpy as np
+
+
+np.random.seed(1)
+oov_vector = np.random.normal(0, 0.1, 300)
+
+model = gensim.models.KeyedVectors.load_word2vec_format('file://C://Users//Henry//Desktop//Masterarbeit//pretrained_embeddings//GoogleNews-vectors-negative300.bin', binary=True)
+
+devtxt = os.path.join(ROOT_FOLDER, 'datasets//MS//transcriptions//dev.txt')
+embeddings_path = os.path.join(ROOT_FOLDER, 'datasets//MS//features//text//embeddings_dev.txt')
+
+txt_lines = open(devtxt).read().splitlines()
+
+
+instance_dict = {}
+
+for line in tqdm.tqdm(txt_lines):
+    instance_matrix = []
+
+    split = line.split('\t')
+    name = split[0]
+
+    for i in range(1, len(split), 2):
+        word = split[i]
+        if word in model.wv:
+            instance_matrix.append(np.array(model.wv[word]))
+        else:
+            instance_matrix.append(oov_vector)
+    instance_matrix = np.vstack(instance_matrix)
+    instance_dict[name] = instance_matrix
+
+np.save(embeddings_path, instance_dict)
--- preprocessing/IEMOCAP/extract_2d_word_embeddings.py
+++ preprocessing/IEMOCAP/extract_2d_word_embeddings.py
@@ -9,7 +9,7 @@ This script uses the recognized words with an existing word level audio and extr
 '''
 
 utterances_with_words = os.path.join(ROOT_FOLDER, 'datasets//IEMOCAP//features//forced_alignment//utterances')
-embeddings_out = os.path.join(ROOT_FOLDER, 'datasets//IEMOCAP//features//text//google_news_word_embeddings_1.npy')
+embeddings_out = os.path.join(ROOT_FOLDER, 'datasets//IEMOCAP//features//text//google_news_word_embeddings.npy')
 model = gensim.models.KeyedVectors.load_word2vec_format('file://C://Users//Henry//Desktop//Masterarbeit//pretrained_embeddings//GoogleNews-vectors-negative300.bin', binary=True)
 
 vector_size = model.vector_size
@@ -17,7 +17,7 @@ vector_size = model.vector_size
 instance_dict = {}
 oov_words= []
 oov_vector = np.random.normal(0, 0.1, 300)
-full_fl = []
+
 for r, d, f in os.walk(utterances_with_words):
     for file in f:
         file_path = os.path.join(r, file)
@@ -32,7 +32,7 @@ for r, d, f in os.walk(utterances_with_words):
                 word = line[:-1].split('\t')[1]
 
                 word = word.lower()
-                word = re.sub(r'\W+', '', word)
+                word = re.sub(r'\'\W+', '', word)
 
                 if word in model.wv:
                     instance_matrix[i, :] = np.array(model.wv[word])
@@ -40,8 +40,6 @@ for r, d, f in os.walk(utterances_with_words):
                     if word not in oov_words:
                         oov_words.append(word)
                     instance_matrix[i, :] = oov_vector
-
-                full_fl.append(instance_matrix[i])
                 i += 1
         instance_dict[name] = instance_matrix
 
